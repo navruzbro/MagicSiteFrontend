72dd1a61b3efb75085bd5733f8b1003bdf43dddc
Navruz BRO <148841747+navruzbro@users.noreply.github.com>
2024-12-08 17:57:20 +0500

    Loading animation and others
    Loading animation and other

--- static/js/main.js
+++ static/js/main.js
@@ -19,3 +19,21 @@ window.addEventListener('scroll', handleScroll);
 
 // Boshlanishda elementlarni tekshirish
 handleScroll();
+
+//Loader animatsiyasi
+document.addEventListener("DOMContentLoaded", function () {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
+
+  // 2 soniyadan keyin loaderni yo'q qilish
+  setTimeout(() => {
+    loader.classList.add("hidden"); // Loaderga animatsiya klassi qo'shildi
+    setTimeout(() => {
+      loader.style.display = "none"; // Loaderni butunlay o'chirish
+      content.style.display = "block"; // Kontentni ko‘rsatish
+    }, 1000); // Animatsiya davomiyligiga mos ravishda kutish
+  }, 2000); // 2 soniya yuklashni kutish
+});
+
+
+
